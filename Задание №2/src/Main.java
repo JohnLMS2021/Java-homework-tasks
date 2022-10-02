55b1a7936f5575982c7e3f9bfe0b8af9b6ccc6ae
JohnLMS2021 <grubyjivan100@gmail.com>
2022-10-02 17:52:25 +0300

    Задание №2

---
+++ Задание №2/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(doTask1(1, 9));
+        doTask2(10);
+        System.out.println(doTask3(-25));
+        doTask4("John", 5);
+
+        System.out.println(doTask5(1900)); // Задание №1*
+    }
+
+//  Задание №1 - Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+    public static boolean doTask1(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <=20) {
+            return true;
+        }
+        return false;
+    }
+
+//  Задание №2 - Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+    public static void doTask2(int x) {
+        if (x >= 0) {
+            System.out.println("Положительное");
+        }
+        else {
+            System.out.println("Отприцательное");
+        }
+    }
+
+// Задание №3 - Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+    public static boolean doTask3(int x) {
+        if (x >= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+//  Задание №4 - Написать метод, которому в качестве аргументов передается строка и число, метод должен отпечатать в консоль указанную строку, указанное количество раз;
+    public static void doTask4(String str, int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println("[" + i + "]" + " " + str);
+        }
+    }
+
+//  Задание №5 - Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    public static boolean doTask5(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
