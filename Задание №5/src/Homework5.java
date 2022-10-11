8be788bcd5448b9982a5edf984d0a5c282f4fc7a
JohnLMS2021 <grubyjivan100@gmail.com>
2022-10-11 09:56:43 +0300

    Домашнее задание №5

---
+++ Задание №5/src/Homework5.java
@@ -0,0 +1,44 @@
+import java.util.EmptyStackException;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        Employee[] empCorp = new Employee[5];
+                empCorp[0] = new Employee("Grubyj Ivan", "Dispatch", "grubyjivan@gmail.com", "17792734188",10000,25);
+                empCorp[1] = new Employee("Mihail Vladov", "Co-owner", "mikevladov@gmail.com", "13252403233",50000,26);
+                empCorp[2] = new Employee("Vainberg Roman", "Co-owner", "vainberg.roman@gmail.com", "17792164600",50000,34);
+                empCorp[3] = new Employee("Xenia Tarnavscaia", "FinancialDirector", "tarnavscaia@gmail.com", "37379761296",35000,31);
+                empCorp[4] = new Employee("Evgheniy Riko", "Supervisior", "rikoevg@gmail.com", "14405307500",20000,33);
+    for (Employee employee : empCorp)
+        if (employee.getAge()>25)
+        System.out.println(employee);
+    }
+}
+class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age) {
+    this.name = name;
+    this.position = position;
+    this.email = email;
+    this.phone = phone;
+    this.salary = salary;
+    this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public String toString() {
+            return (name+ "\n-"
+                    +position+ "\n-"
+                    +email+ "\n-"
+                    +phone+ "\n-"
+                    +salary+ "\n-"
+                    +age);
+    }
+}
