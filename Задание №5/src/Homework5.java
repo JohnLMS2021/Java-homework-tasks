de3b658f0be14425000ba2355a0e16d4565a482f
JohnLMS2021 <96461467+JohnLMS2021@users.noreply.github.com>
2022-10-11 10:01:05 +0300

    Update Homework5.java

--- Задание №5/src/Homework5.java
+++ Задание №5/src/Homework5.java
@@ -4,12 +4,12 @@ public class Homework5 {
     public static void main(String[] args) {
         Employee[] empCorp = new Employee[5];
                 empCorp[0] = new Employee("Grubyj Ivan", "Dispatch", "grubyjivan@gmail.com", "17792734188",10000,25);
-                empCorp[1] = new Employee("Mihail Vladov", "Co-owner", "mikevladov@gmail.com", "13252403233",50000,26);
-                empCorp[2] = new Employee("Vainberg Roman", "Co-owner", "vainberg.roman@gmail.com", "17792164600",50000,34);
-                empCorp[3] = new Employee("Xenia Tarnavscaia", "FinancialDirector", "tarnavscaia@gmail.com", "37379761296",35000,31);
-                empCorp[4] = new Employee("Evgheniy Riko", "Supervisior", "rikoevg@gmail.com", "14405307500",20000,33);
+                empCorp[1] = new Employee("Mihail Vladov", "Co-owner", "mikevladov@gmail.com", "13252403233",50000,36);
+                empCorp[2] = new Employee("Vainberg Roman", "Co-owner", "vainberg.roman@gmail.com", "17792164600",50000,40);
+                empCorp[3] = new Employee("Xenia Tarnavscaia", "FinancialDirector", "tarnavscaia@gmail.com", "37379761296",35000,35);
+                empCorp[4] = new Employee("Evgheniy Riko", "Supervisior", "rikoevg@gmail.com", "14405307500",20000,43);
     for (Employee employee : empCorp)
-        if (employee.getAge()>25)
+        if (employee.getAge()>40)
         System.out.println(employee);
     }
 }
